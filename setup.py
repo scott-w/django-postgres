6d8702530ec0c396f46a49744c70cad74fb5e080
Scott Walton <scw@talktopebble.co.uk>
2013-03-01 14:48:55 +0000

    Updated version number and renamed for use on PyPi

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+LONG_DESCRIPTION = ("First-class Postgre feature support for the Django ORM."
+    "Includes custom extension for manually synching PG Views. For the "
+    "base package, see django-postgres.")
+
 setup(
-    name='django-postgres',
-    version='0.0.2',
+    name='django-postgres-pebble',
+    version='0.0.3',
     description="First-class Postgres feature support for the Django ORM.",
-    author='Zachary Voase',
-    author_email='z@zacharyvoase.com',
+    long_description=LONG_DESCRIPTION,
+    author='Scott Walton',
+    author_email='scw@talktopebble.co.uk',
     license='Public Domain',
     packages=find_packages(),
     install_requires=[
